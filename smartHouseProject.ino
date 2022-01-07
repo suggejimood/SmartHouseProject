b71e2e73127e9ff9da537fbde4bcd7babef4ca31
Mehmet TAN <43840863+suggejimood@users.noreply.github.com>
2022-01-07 11:30:32 +0300

    DHT 11 değişikliği
    Projedeki çalışmayan dht 11 kütüp hanesinin çıkarılması.

--- smartHouseProject.ino
+++ smartHouseProject.ino
@@ -1,14 +1,20 @@
-//-----------------------------------------
-//Arduino DHT11 Web Server using AJAX
-//HTML code of webpage is stored on SD card
-//-----------------------------------------
 #include <SPI.h>
 #include <Ethernet.h>
 #include <SD.h>
-#include <DHT.h>
+#include <dht.h>
+#include <Servo.h>
 //------------------------------------------------
-DHT dht(23, DHT11);
+dht DHT;
+#define DHT11_PIN 7
+
 File HMTL_file;
+Servo bedroom;
+Servo livingroom;
+
+int bed = 9;
+int liv = 10;
+int led1 = 31;
+int led2 = 33;
 //------------------------------------------------
 byte mac[] = {0x90, 0xA2, 0xDA, 0x00, 0x4A, 0xE0};
 IPAddress ip(192, 168, 1, 177);
@@ -17,6 +23,13 @@ EthernetServer server(80);
 void setup()
 {
   Serial.begin(9600);
+
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+
+  bedroom.attach(9);
+  livingroom.attach(10);
+  
   dht.begin();
   pinMode(53, OUTPUT);
   //-----------------------------------------------------
@@ -42,6 +55,7 @@ void setup()
 void loop()
 {
   String HTTP_req;
+  int chk = DHT.read11(DHT11_PIN);
   EthernetClient client = server.available();
   //---------------------------------------------------------------------------
   if(client)
@@ -59,25 +73,59 @@ void loop()
           //-------------------------------------------------------------------
           if(HTTP_req.indexOf("readTemp")>-1)
           {
-            client.println(dht.readTemperature());
+            client.println(DHT.temperature);
             client.println("°C");
             
           }
           else if(HTTP_req.indexOf("readHum")>-1){
-            client.println(int(dht.readHumidity()));
+            client.println(int(DHT.humidity));
             client.println("&nbsp;%");
           }
           else if(HTTP_req.indexOf("led1ON")>-1){
             Serial.println("Led 1 on");
+            digitalWrite(led1, HIGH);
           }
           else if(HTTP_req.indexOf("led1OFF")>-1){
             Serial.println("Led 1 off");
+            digitalWrite(led1, LOW);
           }
           else if(HTTP_req.indexOf("led2ON")>-1){
             Serial.println("Led 2 on");
+            digitalWrite(led2, HIGH);
           }
           else if(HTTP_req.indexOf("led2OFF")>-1){
             Serial.println("Led 2 off");
+            digitalWrite(led2, LOW);
+          }
+          else if(HTTP_req.indexOf("bedroomOpen")>-1){
+            Serial.println("bedroom on");
+            bedroom.write(170);
+            
+          }
+          else if(HTTP_req.indexOf("bedroomHalf")>-1){
+            Serial.println("bedroom half");
+            bedroom.write(90);
+            
+          }
+          else if(HTTP_req.indexOf("bedroomClose")>-1){
+            Serial.println("bedroom Close");
+            bedroom.write(10);
+            
+          }
+          else if(HTTP_req.indexOf("livingroomOpen")>-1){
+            Serial.println("livingroom on");
+            livingroom.write(170);
+ 
+          }
+          else if(HTTP_req.indexOf("livingroomHalf")>-1){
+            Serial.println("livingroom Half");
+            livingroom.write(90);
+ 
+          }
+          else if(HTTP_req.indexOf("livingroomClose")>-1){
+            Serial.println("livingroom Close");
+            livingroom.write(10);
+ 
           }
           else
           {
